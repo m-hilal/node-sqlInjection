40c41ba65ac79a3f3e0cbe8dd1665c8370b15a69
Mohammad Hilla <m.hilal@purevpn.com>
2023-11-30 21:26:37 +0500

    export class

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,3 @@
 import sqlInjectionValidator from "./sqlInjectionValidator";
-import express from "express";
 
-const app = express();
-
-app.use(express.json());
-app.use(sqlInjectionValidator.sqlInjection);
-
-app.post('/sqlinjection', function(req, res, next){
-  res.status(200).send("working");
-})
-
-
-// app.listen(3000, function(){
-//   console.log('app running');
-// });
+export {sqlInjectionValidator};
